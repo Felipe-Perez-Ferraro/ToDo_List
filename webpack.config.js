2db654ae59b3da9714251144ed9d4d29e5da2161
Feliperto <fperezferraro@gmail.com>
2023-07-11 13:33:17 -0300

    Creating the webpack configuration file

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+// HTML
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// CSS
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      minify: false,
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+};
