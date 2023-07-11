7a9bbc1e94665b7ba9e270236604b93e36179dfa
Feliperto <fperezferraro@gmail.com>
2023-07-11 13:32:49 -0300

    Fixing linters errors

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import _ from 'lodash';
 import './style.css';
 
- function component() {
-   const element = document.createElement('div');
+function component() {
+  const element = document.createElement('div');
 
-   // Lodash, now imported by this script
-   element.innerHTML = _.join(['Hello', 'world'], ' ');
-   element.classList.add('hello');
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'world'], ' ');
+  element.classList.add('hello');
 
-   return element;
- }
+  return element;
+}
 
- document.body.appendChild(component());
+document.body.appendChild(component());
