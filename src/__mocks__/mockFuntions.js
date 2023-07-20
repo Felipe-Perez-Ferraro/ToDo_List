1ca4dda6df3bc874cc4c028a23948b90d5a0c3fb
Feliperto <fperezferraro@gmail.com>
2023-07-20 13:50:53 -0300

    creating the mockfuncitons file

---
+++ src/__mocks__/mockFuntions.js
@@ -0,0 +1,13 @@
+export default class ToDo {
+  constructor() {
+    this.arr = [];
+  }
+
+  addTask(task) {
+    this.arr.push(task)
+  }
+
+  removeTask(taskName) {
+    this.arr.splice(0, taskName);
+  }
+}
